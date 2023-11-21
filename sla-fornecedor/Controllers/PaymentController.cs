e320e4c09ac93592bc70ef5c92beab517a13540f
José Romualdo <joseromualdo@outlook.com>
2023-11-21 06:36:50 -0300

    Add project files.

---
+++ sla-fornecedor/Controllers/PaymentController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sla_fornecedor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        public static IDictionary<string, Account> _accounts;
+        private static int _count = 0;
+
+        public PaymentController()
+        {
+            if (_accounts == null)
+                _accounts = new Dictionary<string, Account>();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] PaymentModel payment)
+        {
+            _count++;
+            if (!_accounts.ContainsKey(payment.Account))
+            {
+                _accounts.Add(payment.Account, new Account { Number = payment.Account, Balance = 0 });
+            }
+
+            _accounts[payment.Account].Balance += payment.Ammount;
+
+            if (_count % 2 == 0)
+            {
+                Thread.Sleep(_count * 200);
+                return Ok();
+            }
+
+            if (_count % 3 == 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult<Account> Get(string account)
+        {
+            if (!_accounts.ContainsKey(account))
+            {
+                return NotFound();
+            }
+
+            return Ok(_accounts[account]);
+        }
+    }
+
+    public class Account
+    {
+        public string Number { get; set; }
+        public decimal Balance { get; set; }
+    }
+
+
+    public class PaymentModel
+    {
+        public string Account { get; set; }
+        public decimal Ammount { get; set; }
+    }
+}
---
+++ sla-fornecedor/Controllers/TransferController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sla_fornecedor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+        private static int _count = 0;
+
+        public TransferController()
+        {
+            if(PaymentController._accounts == null)
+            PaymentController._accounts = new Dictionary<string, Account>();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] TransferModel transfer)
+        {
+            _count++;
+            if (!PaymentController._accounts.ContainsKey(transfer.Account))
+            {
+                return BadRequest($"Transfer account {transfer.Account} doesn't exists");
+            }
+
+            if (!PaymentController._accounts.ContainsKey(transfer.DestinationAccount))
+            {
+                return BadRequest($"Destination account {transfer.DestinationAccount} doesn't exists");
+            }
+
+
+            if(PaymentController._accounts[transfer.Account].Balance >= transfer.Ammount)
+            {
+                PaymentController._accounts[transfer.Account].Balance -= transfer.Ammount;
+                PaymentController._accounts[transfer.DestinationAccount].Balance += transfer.Ammount;
+            }
+            else
+            {
+                return BadRequest($"Account {transfer.Account} doesn't have enough balance");
+            }
+
+            if (_count % 2 == 0)
+            {
+                Thread.Sleep(_count * 200);
+                return Ok();
+            }
+
+            if (_count % 3 == 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult<Account> Get(string account)
+        {
+            if (!PaymentController._accounts.ContainsKey(account))
+            {
+                return NotFound();
+            }
+
+            return Ok(PaymentController._accounts[account]);
+        }
+    }
+
+    public class TransferModel
+    {
+        public string Account { get; set; }
+        public string DestinationAccount { get; set; }
+        public decimal Ammount { get; set; }
+    }
+}
